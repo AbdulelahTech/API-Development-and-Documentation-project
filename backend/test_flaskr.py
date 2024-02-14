5c56407b266ad4509fd212cf50a10e1dbc4f4adc
AbdulelahTech <abdulelahalzoman@gmail.com>
2024-02-14 11:25:23 +0300

    Delete unnecessary lines of code

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -1,11 +1,7 @@
-import os
-from traceback import print_tb
 import unittest
-import json
-from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
-from models import setup_db, Question, Category
+from models import Question
 
 
 class TriviaTestCase(unittest.TestCase):
@@ -15,16 +11,6 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        # self.database_name = "trivia_test"
-        # self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
-        # setup_db(self.app, self.database_path)
-
-        # # binds the app to the current context
-        # with self.app.app_context():
-        #     self.db = SQLAlchemy()
-        #     self.db.init_app(self.app)
-        #     # create all tables
-        #     self.db.create_all()
 
     def tearDown(self):
         """Executed after reach test"""
